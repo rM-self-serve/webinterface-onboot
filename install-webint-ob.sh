aaaeca72c48f5a6ae56fbe9596c6c29b90c0e32d
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2025-12-08 18:31:23 +0000

    Update wget to latest

--- install-webint-ob.sh
+++ install-webint-ob.sh
@@ -16,8 +16,8 @@ servicefile="/lib/systemd/system/${pkgname}.service"
 aliasfile="${localbin}/webint-onboot"
 
 wget_path=/home/root/.local/share/rM-self-serve/wget
-wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
-wget_checksum=c258140f059d16d24503c62c1fdf747ca843fe4ba8fcd464a6e6bda8c3bbb6b5
+wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-3
+wget_checksum=3130886a020a56b7471ed60f3742db26acdced4b27fed5be22f9892b77589bc5
 
 main() {
 	case "$@" in
